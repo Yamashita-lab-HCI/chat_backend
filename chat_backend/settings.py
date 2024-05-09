1d6f5ceec07b5688915c2d27ddc2dcc2df938bb7
Senriyama <ksinfo1467@gmail.com>
2024-05-10 01:39:00 +0900

    ログアウト処理も完了！

--- chat_backend/settings.py
+++ chat_backend/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = 'django-insecure-61tdiyux%$mv1e)4o3pum!t6ue$$-w0_ht5)++j(e3&tax^c6y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_COOKIE_HTTPONLY = False
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
