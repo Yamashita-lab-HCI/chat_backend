5cf0e46cf6e6a892e2bdf1ad7b5aedab993191a6
Senriyama <ksinfo1467@gmail.com>
2024-08-22 17:24:16 +0900

    空のメッセージが追加されるバグを修正

--- chat_backend/models.py
+++ chat_backend/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Room(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -21,6 +23,12 @@ class UserProfile(models.Model):
     icon_color = models.CharField(max_length=7, default="#E42222")
 
 class Chat(models.Model):
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=100)
     question = models.TextField()
     answer = models.TextField()
+    purpose = models.CharField(max_length=100, blank=True, null=True)  # 新しいフィールド
+    room = models.CharField(max_length=100, blank=True, null=True)  # 新しいフィールド
+    created_at = models.DateTimeField(default=timezone.now)  # auto_now_add=True を削除
+
+    def __str__(self):
+        return f"{self.username}: {self.question[:50]}..."
